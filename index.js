1220de347ff4a3f369b521023e4a5aa55d9a05a0
Noor Al Darraji <077068@yahoo.com>
2019-09-16 18:15:52 -0500

    Added middleware and end points :hammer:

--- index.js
+++ index.js
@@ -2,6 +2,60 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const server = express();
 
+const restricted = require("./middleware/restrict.js")
+
+server.get("/", (req, res) => {
+    res.send("It's alive!")
+})
+
+server.post("/api/register", (req, res) => {
+    let { username, password } = req.body
+    console.log(username)
+    const hash = bcrypt.hashSync(password, 2)
+
+    Users.add({ username, password: hash })
+        .then(saved => {
+            res
+                .status(201)
+                .json(saved)
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .json(error)
+        })
+})
+
+server.post("/api/login", (req, res) => {
+    let { username, password } = req.body
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                res
+                    .json({ message: `Welcome ${user.username}!` })
+            } else {
+                res
+                    .status(401)
+                    .json({ message: "You cannot pass!" })
+            }
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .json(error)
+        })
+})
+
+server.get("/api/users", restricted, (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => res.send(err))
+})
+
 const port = process.env.PORT || 8000;
 
 server.listen(port, () => console.log(`Listining on port ${port}`));
